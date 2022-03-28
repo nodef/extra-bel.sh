78dbaee91f1365aa77f32280dddb711e573821a8
Subhajit Sahu <wolfram77@gmail.com>
2022-03-28 19:47:39 +0530

    :turtle: fix basic errors

--- index.sh
+++ index.sh
@@ -1,12 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # EBEL - Ring bells
 
 # Options
 n=1
 if [[ "$1" != "" ]]; then n=$1
+fi
 
 # Ring bells
-for i in {1..${n}}; do
-tput bel
-sleep 1
+for (( i = 1; i <= $n; i++ )); do
+  tput bel
+  sleep 1
 done
