d49f9f962f9003f376b0eb3c5e712c7fefe74ae7
Subhajit Sahu <wolfram77@gmail.com>
2022-08-27 22:08:43 +0530

    :mango: update build script

--- build.js
+++ build.js
@@ -1,51 +1,61 @@
-const {fs}              = require('extra-build');
-const {github, package} = require('extra-build');
-
+const build = require('extra-build');
 
 const owner  = 'nodef';
+const repo   = build.readMetadata('.').name;
 
 
 
-// Update GitHub details.
-function updateGithub() {
-  var m = package.read('.');
-  var {name, description} = m;
-  var homepage  = `https://www.npmjs.com/package/${name}`;
-  var topics    = m.keywords;
-  topics.length = Math.min(topics.length, 20);
-  github.updateDetails(owner, name, {description, homepage, topics});
+
+// Get keywords for main/sub package.
+function keywords(ds) {
+  var m = build.readMetadata('.');
+  var s = new Set([...m.keywords, ...ds.map(d => d.name)]);
+  return Array.from(s);
 }
 
 
-// Publish root package to NPM, GitHub.
-function publishRoot(sym, ver) {
-  fs.restoreFileSync('package.json', () => {
-    var m = package.read();
-    m.version  = ver;
-    package.write('.', m);
-    package.publish('.');
-    package.publishGithub('.', owner);
-  });
+// Publish a root package to NPM, GitHub.
+function publishRootPackage(ds, ver, typ) {
+  var _package = build.readDocument('package.json');
+  var m = build.readMetadata('.');
+  m.version  = ver;
+  if (typ) {
+    m.name = `${m.name}.${typ}`;
+    m.description = m.description.replace(/\.$/, ` {${typ}}.`);
+    md = md.replace(/(unpkg\.com\/)(\S+?)(\/\))/, `$1$2.${typ}$3`);
+  }
+  build.writeMetadata('.', m);
+  build.publish('.');
+  try { build.publishGithub('.', owner); }
+  catch {}
+  build.writeDocument(_package);
 }
 
 
 // Deploy root package to NPM, GitHub.
-function deployRoot(ver) {
-  var m   = package.read();
-  publishRoot('', ver);
+function publishRootPackages(ds, ver) {
+  publishRootPackage(ds, ver, '');
 }
 
 
-// Perform all deploy work.
-function deployAll() {
-  var m   = package.read();
-  var ver = package.nextUnpublishedVersion(m.name, m.version);
-  updateGithub();
-  deployRoot(ver);
+// Publish docs.
+function publishDocs(ds) {
+  build.updateGithubRepoDetails({topics: keywords(ds)});
 }
 
 
+// Pushish root, sub packages to NPM, GitHub.
+function publishPackages(ds) {
+  var m   = build.readMetadata('.');
+  var ver = build.nextUnpublishedVersion(m.name, m.version);
+  publishRootPackages(ds, ver);
+}
+
+
+// Finally.
 function main(a) {
-  if (a[2] === 'deploy') deployAll();
+  var ds = [];
+  if (a[2]==='publish-docs') publishDocs(ds);
+  else if (a[2]==='publish-packages') publishPackages(ds);
 }
 main(process.argv);
